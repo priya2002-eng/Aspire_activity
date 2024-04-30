9b83488f4c38747174f0555b9a6ccfa4fa70fbca
priya2002-eng <124424389+priya2002-eng@users.noreply.github.com>
2024-04-30 20:31:57 +0530

    Update event.js

--- Node Js/event.js
+++ Node Js/event.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Title - Implementing Event Emitter module of Node Js
 // Author - Priyadharshini S
 // Created Date - 28/04/2024
@@ -16,22 +15,3 @@ const myEventHandler = () => {
 myEmitter.on('myEvent', myEventHandler);
 
 myEmitter.emit('myEvent');
-=======
-// Title - Implementing Event Emitter module of Node Js
-// Author - Priyadharshini S
-// Created Date - 28/04/2024
-
-const EventEmitter = require('events');
-
-class MyEmitter extends EventEmitter {}
-
-const myEmitter = new MyEmitter();
-
-const myEventHandler = () => {
-  console.log('Event occurred!');
-};
-
-myEmitter.on('myEvent', myEventHandler);
-
-myEmitter.emit('myEvent');
->>>>>>> 7e34a59c72ecdf000644485e1406dd9a514fbec4
