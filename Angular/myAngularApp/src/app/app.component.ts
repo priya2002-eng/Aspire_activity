ccf6e757df2fa49d3a423b0e4b47138623f77ee1
priya2002-eng <124424389+priya2002-eng@users.noreply.github.com>
2024-05-19 20:40:44 +0530

    Update app.component.ts

--- Angular/myAngularApp/src/app/app.component.ts
+++ Angular/myAngularApp/src/app/app.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { StudentComponent } from './student/student.component';
 import { FormsModule } from '@angular/forms';
 import { ShopComponent } from './shop/shop.component';
 import { ProductComponent } from './product/product.component';
 import { AccountComponent } from './account/account.component';
+import { CommonModule } from '@angular/common';
+import { CubePipe } from './cube.pipe';
+import { GenderPipe } from './gender.pipe';
+import { ShirtSizePipe } from './shirt-size.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule,AccountComponent],
+  imports: [RouterOutlet, FormsModule,CubePipe,GenderPipe,ShirtSizePipe,AccountComponent, RouterLink,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'myAngularApp';
+  joinDate: Date= new Date();
 }
