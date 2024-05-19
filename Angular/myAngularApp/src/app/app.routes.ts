df5e140a09933ba76f48674d8a8aa6a4c6232296
priya2002-eng <124424389+priya2002-eng@users.noreply.github.com>
2024-05-19 20:41:16 +0530

    Update app.routes.ts

--- Angular/myAngularApp/src/app/app.routes.ts
+++ Angular/myAngularApp/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { ShopComponent } from './shop/shop.component';
+import { StudentComponent } from './student/student.component';
+import { ProductComponent } from './product/product.component';
+import { AccountComponent } from './account/account.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'shop',
+        component: ShopComponent
+    },
+    {
+        path: 'student',
+        component: StudentComponent
+    },
+    {
+        path: 'product',
+        component: ProductComponent
+    },
+    {
+        path: 'account',
+        component: AccountComponent
+    },
+    {
+        path: '**',
+        component: AccountComponent
+    }
+];
