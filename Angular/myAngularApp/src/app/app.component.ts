7a4e1fcb01599dbc3fc9c8591319a827bd2dcdef
priya2002-eng <124424389+priya2002-eng@users.noreply.github.com>
2024-05-23 10:54:01 +0530

    Update app.component.ts

--- Angular/myAngularApp/src/app/app.component.ts
+++ Angular/myAngularApp/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { StudentComponent } from './student/student.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,  ReactiveFormsModule } from '@angular/forms';
 import { ShopComponent } from './shop/shop.component';
 import { ProductComponent } from './product/product.component';
 import { AccountComponent } from './account/account.component';
@@ -10,11 +10,13 @@ import { CubePipe } from './cube.pipe';
 import { GenderPipe } from './gender.pipe';
 import { ShirtSizePipe } from './shirt-size.pipe';
 import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductdetComponent } from './productdet/productdet.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule,CubePipe,GenderPipe,ShirtSizePipe,AccountComponent, RouterLink,CommonModule,HomeComponent],
+  imports: [RouterOutlet,HttpClientModule, FormsModule,CubePipe,GenderPipe,ReactiveFormsModule,ShirtSizePipe,AccountComponent,ProductdetComponent, RouterLink,CommonModule,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
