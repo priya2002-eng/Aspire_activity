81107368b79a013a5c9b67d4649fd88e0ba8578d
priya2002-eng <124424389+priya2002-eng@users.noreply.github.com>
2024-05-23 18:16:53 +0530

    Delete Employee Leave Management System directory

--- Employee Leave Management System/apply_leave.js
+++
@@ -1,8 +0,0 @@
-function logout() {
-    // Redirect to index.html
-    window.location.href = "index.html";
-  }
-  
-  document.getElementById("logoutBtn").addEventListener("click", function() {
-    logout();
-  });
