861246b8dbdf670dd9f085418d4f75ef3ff90c77
Priyadharshini <124424389+priya2002-eng@users.noreply.github.com>
2024-09-10 11:15:09 +0530

    Delete ReactJs ELMS directory

--- ReactJs ELMS/backend/models/LeaveType.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const LeaveTypeSchema = new mongoose.Schema({
-  leaveType: { type: String, required: true },
-  description: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('LeaveType', LeaveTypeSchema);
